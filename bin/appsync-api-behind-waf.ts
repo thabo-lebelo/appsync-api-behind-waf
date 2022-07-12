ef40c81ec568d3448c3970207b91d34cfd40c30f
Thabo Lebelo <thabolebelo@icloud.com>
2022-07-12 20:32:19 +0200

    Code refactor

--- bin/appsync-api-behind-waf.ts
+++ bin/appsync-api-behind-waf.ts
@@ -1,25 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { AppsyncApiBehindWafStack } from '../lib/appsync-api-behind-waf-stack';
 
 import { AppsyncAPI } from '../lib/appsync-api'
 import { WebAppFirewall } from '../lib/web-app-firewall'
 
-// const app = new cdk.App();
-// new AppsyncApiBehindWafStack(app, 'AppsyncApiBehindWafStack', {
-//     stackName: "appsync-api-behind-waf ",
-//     description: "Deploying an AWS AppSync API with WAF using CDK v2"
-// });
+const app = new App();
 
-function createCloudFormation(): void {
-    const app = new App();
+const appSync = new AppsyncAPI(app, 'Backend');
 
-    const appSync = new AppsyncAPI(app, 'Backend');
-
-    new WebAppFirewall(app, 'Firewall', {
-        apiArn: appSync.api.attrArn,
-    });
-}
-
-createCloudFormation();
+new WebAppFirewall(app, 'Firewall', {
+    apiArn: appSync.api.attrArn,
+});
